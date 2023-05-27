a263416b11b0e9e10994dc1675de4f1da0fb2c65
Heather Greenberg <hgreenberg1017@gmail.com>
2023-05-27 12:14:32 -0700

    fix minor errors

--- db/schema.sql
+++ db/schema.sql
@@ -11,18 +11,18 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
     department_id INT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (department_id)
-    REFERENCES department(id)
+    REFERENCES department (id)
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
+    role_id INT NOT NULL,
     manager_id INT,
     PRIMARY KEY(id),
     FOREIGN KEY (manager_id) 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,28 +1,28 @@
 USE employee_tracker;
 
 INSERT INTO department (name)
-VALUES ("CUSTOMER SUPPORT"),
-       ("MARKETING"),
-       ("SALES"),
-       ("ENGINEERING");
+VALUES ('CUSTOMER SUPPORT'),
+       ('MARKETING'),
+       ('SALES'),
+       ('ENGINEERING');
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Support Manager", 90000, 1),
-       ("Support Specialist", 80000, 1),
-       ("Marketing Manager", 100000, 2),
-       ("Marketing Spealist", 70000, 2),
-       ("Sales Manager", 110000, 3),
-       ("Sales Lead", 60000, 3),
-       ("Front End Developer", 120000, 4),
-       ("Back End Developer", 130000, 4);
+VALUES ('Support Manager', 90000, 1),
+       ('Support Specialist', 80000, 1),
+       ('Marketing Manager', 100000, 2),
+       ('Marketing Spealist', 70000, 2),
+       ('Sales Manager', 110000, 3),
+       ('Sales Lead', 60000, 3),
+       ('Front End Developer', 120000, 4),
+       ('Back End Developer', 130000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id)
-VALUES ("Ben", "Baker", 9),
-       ("Leslie", "Lynn", 8),
-       ("Frank", "Fern", 7 ),
-       ("Joe", "Smith", 6),
-       ("Lindsey", "Jones", 5),
-       ("Hanna", "Brown", 4),
-       ("Heather", "Greenberf", 3),
-       ("Sarah", "Johnson", 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Ben', 'Baker', 9, NULL),
+       ('Leslie', 'Lynn', 8, 1 ),
+       ('Frank', 'Fern', 7, 2 ),
+       ('Joe', 'Smith', 6, 3 ),
+       ('Lindsey', 'Jones', 5, 4 ),
+       ('Hanna', 'Brown', 4, 5 ),
+       ('Heather', 'Greenberg', 3, 6 ),
+       ('Sarah', 'Johnson', 2, 7 );
 
